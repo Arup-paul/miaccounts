ae24b83177e04c9801d0671e2ff3b18b92ef3f43
arup-paul <aruppaulctg@gmail.com>
2024-06-28 23:40:08 +0600

    installation documentation

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,7 +9,7 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="{{asset('asset/js/tailwindcss.js')}}"></script>
 
 </head>
 <body class="font-sans antialiased dark:bg-black dark:text-white/50">
@@ -17,9 +17,6 @@
     <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
         <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
             @yield('content')
-
-
-
 
         </div>
     </div>
